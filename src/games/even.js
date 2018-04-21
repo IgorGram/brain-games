ab69aad26aabfe8d49f19734fdcfd9816cd7a35b
Igor Grammatchikov <grammatchikov@inbox.ru>
2018-04-21 22:21:04 +0300

    add new logic with using pairs

--- src/games/even.js
+++ src/games/even.js
@@ -1,67 +1,19 @@
-import readlineSync from 'readline-sync';
+import { cons } from 'hexlet-pairs';
 import runGame from '..';
 
-let randomNumber;
-let userAnswer;
-let correctAnswer;
-
-// get random number in range
-const getRandomNumber = (min, max) => {
-  randomNumber = min + (Math.random() * ((max + 1) - min));
+// get pair of question and correct answer
+const getGamePair = () => {
+  const min = 1;
+  const max = 20;
+  let randomNumber = min + (Math.random() * ((max + 1) - min));
   randomNumber = Math.floor(randomNumber);
-  return randomNumber;
+  const correctAnswer = randomNumber % 2 ? 'no' : 'yes';
+  return cons(randomNumber, correctAnswer);
 };
-
-// question
-const question = () => {
-  console.log(`Question: ${getRandomNumber(1, 20)}`);
-};
-
-// getCorrectAnswer
-const gerCorrectAnswer = () => {
-  correctAnswer = randomNumber % 2 ? 'no' : 'yes';
-};
-
-// wrongAnswer
-const wrongAnswer = (userName) => {
-  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-  console.log(`Let's try again, ${userName}`);
-};
-
-// userAnswer
-const getUserAnswer = () => {
-  userAnswer = readlineSync.question('Your answer: ');
-};
-
-// isCorrectTypeAnswer
-const isCorrectTypeAnswer = () => userAnswer !== 'yes' || userAnswer !== 'no';
-
-// getQuestionSection
-const getQuestionSection = (counter, userName) => {
-  if (counter === 0) {
-    console.log(`Congratulations, ${userName}`);
-    return;
-  }
-  question();
-  gerCorrectAnswer();
-  getUserAnswer();
-  if (!isCorrectTypeAnswer()) {
-    wrongAnswer(userName);
-    return;
-  }
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-  } else {
-    wrongAnswer(userName);
-    return;
-  }
-  getQuestionSection(counter - 1);
-};
-
-// summary func evenGame
+// start game
 const startEvenGame = () => {
   const gameName = 'Answer "yes" if number even otherwise answer "no".';
-  runGame(gameName, getQuestionSection);
+  runGame(gameName, getGamePair);
 };
 
 export default startEvenGame;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import readlineSync from 'readline-sync';
+import { car, cdr } from 'hexlet-pairs';
 
-const runGame = (gameName, getQuestionSection) => {
+
+const runGame = (gameName, getQuestionPair) => {
+  let count = 3;
   // greeting the gamer and show the name of game(question)
   console.log('Welcome to the Brain Games!');
   console.log(`${gameName}\n`);
   // ask user name
   const userName = readlineSync.question('May I have your name? ');
-  // print greeting
   console.log(`Hello, ${userName}!\n`);
-  // ask question
-  if (getQuestionSection) {
-    getQuestionSection(3, userName);
+  // 3 round of game
+  while (count > 0) {
+    const pair = getQuestionPair();
+    const question = car(pair);
+    const correctAnswer = cdr(pair);
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}`);
+      return;
+    }
+    console.log('Correct!');
+    count -= 1;
   }
+  console.log(`Congratulations, ${userName}`);
 };
 
 export default runGame;
